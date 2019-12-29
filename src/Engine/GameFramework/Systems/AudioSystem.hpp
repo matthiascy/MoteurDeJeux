049d4cc7351a0403fa27c652d810deb33cacfe75
cyprien.maison@etu.umontpellier.fr <cyprien.maison@etu.umontpellier.fr@prodpeda-x2go-bionic3.fdsetu.infra.umontpellier.fr>
2019-12-29 22:07:46 +0100

    Tests et debuggage du moteur audio termine.

--- src/Engine/GameFramework/Systems/AudioSystem.hpp
+++ src/Engine/GameFramework/Systems/AudioSystem.hpp
@@ -1,9 +1,8 @@
 #ifndef AUDIOSYSTEM_H
 #define AUDIOSYSTEM_H
 
-#include "../../Thirdparty/fmod/fmod.hpp"
-#include "../../Thirdparty/fmod/fmod_studio.hpp"
-#include "QVector3D"
+#include "Thirdparty/fmod/fmod.hpp"
+#include "Thirdparty/fmod/fmod_studio.hpp"
 #include "map"
 #include "string"
 #include "../System.hpp"
@@ -13,19 +12,34 @@ class AudioSystem : public System
 public:
     AudioSystem(const String& name, Engine* engine, Object* parent = nullptr);
     ~AudioSystem();
-    void jouerSon(std::string nomFichier);
-    void setParam(std::string nomEvent, std::string nomParam, float valParam){m_mapEvents[nomEvent]->setParameterByName(nomParam.c_str(), valParam);}
+    void chargerSon(std::string nomAMapper, std::string nomFichier);
+    void jouerSon(std::string nomMappe);
+    void dechargerSon(std::string nomMappe){FMOD_Sound_Release(m_sons[nomMappe]);}
+    void setParam(std::string nomEvent, std::string nomParam, float valParam){FMOD_Studio_EventInstance_SetParameterByName(m_mapEvents[nomEvent],nomParam.c_str(), valParam, false);}
     float getParam(std::string nomEvent, std::string nomParam);
     void chargerBanque(std::string nomAMapper, std::string nomFichier);
-    void chargerEvent(std::string nomEvent);
+    void dechargerBanque(std::string nomMappe){FMOD_Studio_Bank_Unload(m_banques[nomMappe]);}
+    void chargerEvent(std::string nomEvent,std::string pathFmod);//premier paramètre est le nom à mapper, le deuxième et le chemin que on peu copier de FMOD. Note : il faut charger la banque de strings (Master.strings.bank), sous n'importe quel nom.
     void jouerEvent(std::string nomEvent);
     void stopperEvent(std::string nomEvent);
+    void dechargerEvent(std::string nomMappe){FMOD_Studio_EventInstance_Release(m_mapEvents[nomMappe]);}
+
+    void init() override;
+
+    void fixedUpdate(Real dt) override { };
+
+    void preUpdate(Real dt) override { };
+
+    void update(Real dt) override { };
+
+    void postUpdate(Real dt) override { };
 
 protected :
     FMOD_SYSTEM* m_system;
-    FMOD::Studio::System* m_studio;
-    std::map<std::string, FMOD::Studio::EventInstance*> m_mapEvents;
-    std::map<std::string, FMOD::Studio::Bank*> m_banques;
+    FMOD_STUDIO_SYSTEM* m_studio;
+    std::map<std::string, FMOD_STUDIO_EVENTINSTANCE*> m_mapEvents;
+    std::map<std::string, FMOD_STUDIO_BANK*> m_banques;
+    std::map<std::string, FMOD_SOUND*> m_sons;
 };
 
 #endif // AUDIOSYSTEM_H
--- src/Engine/GameFramework/Systems/Private/AudioSystem.cpp
+++ src/Engine/GameFramework/Systems/Private/AudioSystem.cpp
@@ -1,66 +1,83 @@
-#include "GameFramework/Systems/AudioSystem.hpp"
+#include "../AudioSystem.hpp"
 
 AudioSystem::AudioSystem(const String &name, Engine *engine, Object *parent) : System(name, engine, parent)
 {
     FMOD_System_Create(&m_system);
     FMOD_System_Init(m_system, 32, FMOD_INIT_NORMAL, NULL);
 
-    FMOD::Studio::System::create(&m_studio,FMOD_VERSION);
-    m_studio->initialize(32, FMOD_STUDIO_INIT_NORMAL, FMOD_INIT_NORMAL, NULL);
+    FMOD_Studio_System_Create(&m_studio,FMOD_VERSION);
+    FMOD_Studio_System_Initialize(m_studio,32, FMOD_STUDIO_INIT_NORMAL, FMOD_INIT_NORMAL, NULL);
 
 }
 
 AudioSystem::~AudioSystem(){
 
+    for(auto const& son : m_sons){
+        dechargerSon(son.first);
+    }
+
+    for(auto const& event : m_mapEvents){
+        dechargerEvent(event.first);
+    }
+
+    for(auto const& banque : m_banques){
+        dechargerBanque(banque.first);
+    }
+
     FMOD_System_Close(m_system);
     FMOD_System_Release(m_system);
 
-    m_studio->release();
+    FMOD_Studio_System_Release(m_studio);
 }
 
-void AudioSystem::jouerSon(std::string nomFichier){
+void AudioSystem::chargerSon(std::string nomAMapper, std::string nomFichier){
 
     FMOD_SOUND *son = NULL;
     FMOD_System_CreateSound(m_system, nomFichier.c_str(), FMOD_CREATESAMPLE, 0, &son);
-    FMOD_System_PlaySound(m_system, son, NULL, 0, NULL);
-    FMOD_Sound_Release(son);
+    if(son != NULL) m_sons[nomAMapper] = son;
+}
 
+void AudioSystem::jouerSon(std::string nomMappe){
+    FMOD_System_PlaySound(m_system, m_sons[nomMappe], NULL, 0, NULL);
 }
 
 float AudioSystem::getParam(std::string nomEvent, std::string nomParam){
     float ret;
-    m_mapEvents[nomEvent]->getParameterByName(nomParam.c_str(), &ret);
+    FMOD_Studio_EventInstance_GetParameterByName(m_mapEvents[nomEvent], nomParam.c_str(), &ret, NULL);
     return ret;
 }
 
 void AudioSystem::chargerBanque(std::string nomAMapper, std::string nomFichier){
 
-    FMOD::Studio::Bank* banque = NULL;
-    m_studio->loadBankFile(nomFichier.c_str(), FMOD_STUDIO_LOAD_BANK_NORMAL, &banque) ;
+    FMOD_STUDIO_BANK* banque = NULL;
+    FMOD_Studio_System_LoadBankFile(m_studio, nomFichier.c_str(), FMOD_STUDIO_LOAD_BANK_NORMAL, &banque);
     m_banques[nomAMapper] = banque;
 
 }
 
-void AudioSystem::chargerEvent(std::string nomEvent){
+void AudioSystem::chargerEvent(std::string nomEventAMapper, std::string pathFmod){
 
-    FMOD::Studio::EventDescription* desc = NULL;
-    m_studio->getEvent(nomEvent.c_str(), &desc);
+    FMOD_STUDIO_EVENTDESCRIPTION* desc = NULL;
+    FMOD_Studio_System_GetEvent(m_studio, pathFmod.c_str(), &desc);
     if(desc != NULL){
-        FMOD::Studio::EventInstance* instance = NULL;
-        desc->createInstance(&instance);
-        if(instance != NULL) m_mapEvents[nomEvent]=instance;
+        FMOD_STUDIO_EVENTINSTANCE* instance = NULL;
+        FMOD_Studio_EventDescription_CreateInstance(desc, &instance);
+        if(instance != NULL) m_mapEvents[nomEventAMapper]=instance;
     }
 
 }
 
 void AudioSystem::jouerEvent(std::string nomEvent){
 
-    m_mapEvents[nomEvent]->start();
+    FMOD_Studio_System_FlushCommands(m_studio);
+    FMOD_Studio_EventInstance_Start(m_mapEvents[nomEvent]);
+    FMOD_Studio_System_FlushCommands(m_studio);
 
 }
-
 void AudioSystem::stopperEvent(std::string nomEvent){
 
-    m_mapEvents[nomEvent]->stop(FMOD_STUDIO_STOP_IMMEDIATE);
+    FMOD_Studio_System_FlushCommands(m_studio);
+    FMOD_Studio_EventInstance_Stop(m_mapEvents[nomEvent], FMOD_STUDIO_STOP_IMMEDIATE);
+    FMOD_Studio_System_FlushCommands(m_studio);
 
 }
